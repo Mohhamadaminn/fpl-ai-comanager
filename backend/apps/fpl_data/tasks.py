aa9c7ec57c7a4bc84ea635de76add06f940a0bd9
Mohhamadaminn <mohamad4aminm@gmail.com>
2026-08-20 14:07:31 +0330

    create tasks.py

---
+++ backend/apps/fpl_data/tasks.py
@@ -0,0 +1,16 @@
+# apps/fpl_data/tasks.py
+from celery import shared_task
+from .services import sync_bootstrap_data, sync_live_gameweek_stats
+from .models import Gameweek
+
+
+@shared_task
+def sync_bootstrap_task():
+    sync_bootstrap_data()
+
+
+@shared_task
+def sync_live_stats_task():
+    current_gw = Gameweek.objects.filter(is_current=True).first()
+    if current_gw:
+        sync_live_gameweek_stats(current_gw.fpl_id)
--- backend/config/settings.py
+++ backend/config/settings.py
@@ -47,6 +47,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
+    'django_celery_beat',
+    'corsheaders',
+
+    'fpl_data',
 ]
 
 MIDDLEWARE = [

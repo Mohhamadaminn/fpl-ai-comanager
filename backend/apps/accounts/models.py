1bbaf670af0e52f90d9df5bfa99749c7e9f760b7
Mohhamadaminn <mohamad4aminm@gmail.com>
2026-08-23 02:07:31 +0330

    define accounts model

--- backend/apps/accounts/models.py
+++ backend/apps/accounts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class FPLManagerProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    fpl_team_id = models.PositiveIntegerField(unique=True)
+
+    def __str__(self):
+        return f"{self.user.username} - Team {self.fpl_team_id}"
